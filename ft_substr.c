6105d80ed03a0ad0829b7eef4098b8f365803970
Erik Cousillas Fernandez <ecousill@c3r3s6.42urduliz.com>
2024-09-25 16:41:53 +0200

    jeh

--- ft_substr.c
+++ ft_substr.c
@@ -41,7 +41,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s))
+	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
 	if (len > ft_strlen(s + start))
 		len = ft_strlen(s + start);
