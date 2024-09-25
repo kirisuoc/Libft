5082094e2bb643c942bddac1ce79949d940490bf
kirisuoc <erikcousillas@icloud.com>
2024-09-25 16:22:08 +0200

    First try Libft

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecousill <ecousill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 19:21:22 by erikcousill       #+#    #+#             */
+/*   Updated: 2024/09/23 18:35:59 by ecousill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+	size_t				i;
+
+	str1 = s1;
+	str2 = s2;
+	i = 0;
+	while (*str1 == *str2 && i < n)
+	{
+		i++;
+		str1++;
+		str2++;
+	}
+	if (i == n)
+		return (0);
+	return (*str1 - *str2);
+}
---
+++ ft_substr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecousill <ecousill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 12:24:09 by ecousill          #+#    #+#             */
+/*   Updated: 2024/09/23 15:17:58 by ecousill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	i;
+
+	if (s[start] >= ft_strlen(s))
+		return (NULL);
+	subs = malloc(len + 1);
+	if (subs == NULL)
+		return (NULL);
+	i = 0;
+	while (s[start] != '\0' && i < len)
+	{
+		subs[i] = s[start];
+		start++;
+		i++;
+	}
+	subs[i] = '\0';
+	return (subs);
+}
